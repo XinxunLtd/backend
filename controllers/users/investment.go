c25856081872e878a75277b064168da307596b76
CiroosAI <ciroosai@gmail.com>
2025-11-24 08:51:04 +0700

    Fixing

--- controllers/users/investment.go
+++ controllers/users/investment.go
@@ -930,7 +930,6 @@ func createKytaQRISSafe(ctx context.Context, client *http.Client, baseURL, acces
 		"notify_url":   notifyURL,
 		"success_url":  successURL,
 		"failed_url":   failedURL,
-		"expires_time": 86400,
 	}
 	body, _ := json.Marshal(payload)
 
@@ -994,7 +993,6 @@ func createKytaVASafe(ctx context.Context, client *http.Client, baseURL, accessT
 		"notify_url":   notifyURL,
 		"success_url":  successURL,
 		"failed_url":   failedURL,
-		"expires_time": 86400,
 	}
 	body, _ := json.Marshal(payload)
 
