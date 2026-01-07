84cd8039213dde9d2f3a6b17e333c4988ecdc635
CiroosAI <ciroosai@gmail.com>
2026-01-07 23:36:29 +0700

    Commit

--- controllers/admins/withdrawals.go
+++ controllers/admins/withdrawals.go
@@ -580,7 +580,7 @@ func KytaPayoutCallbackHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS")
 	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
-	
+
 	// Handle OPTIONS preflight
 	if r.Method == http.MethodOptions {
 		w.WriteHeader(http.StatusNoContent)
